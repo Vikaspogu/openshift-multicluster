bb243119adf25f95559d3206212e103747617035
Vikas Pogu <vikaspoguadf@gmail.com>
2024-09-12 11:43:17 -0400

    fix namde

--- .bootstrap/bootstrap.sh
+++ .bootstrap/bootstrap.sh
@@ -3,4 +3,4 @@ export KUBECONFIG=./kubeconfig
 oc apply -k ../components/openshift-gitops-operator
 sleep 60s
 cat ~/.config/sops/age/keys.txt | oc create secret generic sops-age -n openshift-gitops --from-file=keys.txt=/dev/stdin
-oc apply -k . -n openshift-gitops
+oc apply -k ../components/openshift-gitops-config -n openshift-gitops
