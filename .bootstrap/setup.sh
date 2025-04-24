8b06f08657ab69c27adad50369bd4dae772a1a94
Vikas Pogu <vikaspoguadf@gmail.com>
2025-04-24 13:26:14 -0400

    (fix): update argocd

--- .bootstrap/setup.sh
+++ .bootstrap/setup.sh
@@ -3,4 +3,4 @@ export KUBECONFIG=./kubeconfig
 oc apply -k ../components/openshift-gitops-operator
 sleep 60s
 cat ~/.config/sops/age/keys.txt | oc create secret generic sops-age -n openshift-gitops --from-file=keys.txt=/dev/stdin
-oc apply -k ../components/acm-policies-config/gitops-config/manifests -n openshift-gitops
+oc apply -k ../components/openshift-gitops-config -n openshift-gitops
