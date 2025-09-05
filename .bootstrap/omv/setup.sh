69842ebebe3956493c0e308824de068d0af451e4
Vikas Pogu <vikaspoguadf@gmail.com>
2025-09-05 07:53:10 -0400

    (feat): add new k3s cluster

---
+++ .bootstrap/omv/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export KUBECONFIG=./kubeconfig
+oc apply -k ../../components/argocd-chart -n argocd-app
+cat ~/.config/sops/age/keys.txt | oc create secret generic helm-secrets-private-keys -n argocd-app --from-file=keys.txt=/dev/stdin
+oc apply -k .
