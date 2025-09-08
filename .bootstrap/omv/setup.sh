ea46b1254abcf6d0f770210ef4210b685810410a
Vikas Pogu <vikaspoguadf@gmail.com>
2025-09-08 12:04:20 -0400

    (fix): namepsace

--- .bootstrap/omv/setup.sh
+++ .bootstrap/omv/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export KUBECONFIG=./kubeconfig
-oc apply -k ../../components/argocd-chart -n argocd-app
-cat ~/.config/sops/age/keys.txt | oc create secret generic helm-secrets-private-keys -n argocd-app --from-file=keys.txt=/dev/stdin
-oc create secret generic environment-variables -n argocd-app
+oc apply -k ../../components/argocd-chart -n argo-system
+cat ~/.config/sops/age/keys.txt | oc create secret generic helm-secrets-private-keys -n argo-system --from-file=keys.txt=/dev/stdin
+oc create secret generic environment-variables -n argo-system
 oc apply -k .
