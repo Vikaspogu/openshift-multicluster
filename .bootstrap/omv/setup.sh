56a12bd96fd6cf01af7dd71caaf6aac04630c8bd
Vikas Pogu <vikaspoguadf@gmail.com>
2025-09-08 11:34:49 -0400

    (feat): add exytracontainer

--- .bootstrap/omv/setup.sh
+++ .bootstrap/omv/setup.sh
@@ -2,4 +2,5 @@
 export KUBECONFIG=./kubeconfig
 oc apply -k ../../components/argocd-chart -n argocd-app
 cat ~/.config/sops/age/keys.txt | oc create secret generic helm-secrets-private-keys -n argocd-app --from-file=keys.txt=/dev/stdin
+oc create secret generic environment-variables -n argocd-app
 oc apply -k .
